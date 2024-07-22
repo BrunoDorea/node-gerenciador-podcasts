b86a92b410422b0c2cbb258bc75b28b17867f200
Bruno Henrique <brunodorea@outlook.com.br>
2024-07-22 16:00:57 -0300

    :recycle: refactor: tratamento de erros

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -2,23 +2,27 @@ import { IncomingMessage, ServerResponse } from "http";
 
 import { serviceListEpisodes } from "../services/list-episodes-service";
 import { serviceFilterEpisodes } from "../services/filter-episodes-service";
-import { StatusCode } from "../utils/status-code";
 import { ContentType } from "../utils/content-type";
+import { PodcastTransferModel } from "../models/podcast-transfer-model";
+
+const DEFAULT_CONTENT = { "Content-Type": ContentType.JSON };
 
 export const getListEpisodes = async (
     req: IncomingMessage,
     res: ServerResponse
 ) => {
-    const content = await serviceListEpisodes();
-    res.writeHead(StatusCode.OK, { "Content-Type": ContentType.JSON });
-    res.end(JSON.stringify(content));
+    const content: PodcastTransferModel = await serviceListEpisodes();
+    res.writeHead(content.statusCode, DEFAULT_CONTENT);
+    res.write(JSON.stringify(content.body));
+    res.end();
 };
 
 export const getFilterEpisodes = async (
     req: IncomingMessage,
     res: ServerResponse
 ) => {
-    const content = await serviceFilterEpisodes(req.url);
-    res.writeHead(StatusCode.OK, { "Content-Type": ContentType.JSON });
-    res.end(JSON.stringify(content));
+    const content: PodcastTransferModel = await serviceFilterEpisodes(req.url);
+    res.writeHead(content.statusCode, DEFAULT_CONTENT);
+    res.write(JSON.stringify(content.body));
+    res.end();
 };
